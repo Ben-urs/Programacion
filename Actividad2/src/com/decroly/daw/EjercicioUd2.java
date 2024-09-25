6e995fa6d3e3c294488e53224060f44074d20de7
COMPU-S029\daw1 <beniaminpatras@gmail.com>
2024-09-25 11:32:46 +0200

    Ej Actividad2

---
+++ Actividad2/src/com/decroly/daw/EjercicioUd2.java
@@ -0,0 +1,58 @@
+package com.decroly.daw;
+
+public class EjercicioUd2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner (System.in);
+		
+		System.out.println("Escribe la edad");
+		
+		int nume, mayor, menor;
+		
+		if (mayor >= 18)
+		System.out.println("Eres menor de edad");
+		
+		if (menor <18)
+		System.out.println("Eres menor de edad");
+		
+		sc = new Scanner (System.in);
+		
+		System.out.println("Dame los numeros del 1 al 20");
+		
+	
+		
+		int cont;
+		
+		for (cont=1;cont<20;cont++)
+			System.out.print(cont + "");
+		
+		System.out.print("\n");
+		
+		
+		int pares, cont;
+		
+		pares = 0;
+		
+		for (cont = 100; cont <= 200; cont++ )
+			
+		 {
+				if(cont % 2 == 0);
+					pares == pares
+				else
+					
+					
+					
+					
+					
+	}			
+			
+		
+			
+			
+		
+	
+			
+	} //main
+
+} //clase
