9689323f9236fee11103fdbcb9742bc9f4ecd2ea
Ben-urs <beniaminpatras@gmail.com>
2024-10-11 06:58:33 +0200

    Update UD2Actividad3.java

--- UD2_Actividad_03/src/com/decroly/daw/UD2Actividad3.java
+++ UD2_Actividad_03/src/com/decroly/daw/UD2Actividad3.java
@@ -7,7 +7,7 @@ public class UD2Actividad3 {
 	public static void main(String[] args) {
 						
 		String opcion ="";
-		   Scanner reader = new Scanner(System.in);		
+		   Scanner scanner = new Scanner(System.in);		
 		   		   		
 		do {
 			System.out.println("Inserta el numero 1 para Sumar");
@@ -107,8 +107,116 @@ public class UD2Actividad3 {
 
 
 		}while(!opcion.equals("5"));	
-	}
-}
+	
+
+		
+ System.out.print("Introduce una cantidad en euros (múltiplo de 5): ");
+        int cantidad = scanner.nextInt();
+
+        
+        if (cantidad % 5 != 0) {
+            System.out.println("La cantidad debe ser un múltiplo de 5.");
+            scanner.close();
+            return; 
+        }
+
+        
+        int billetes500 = 0;
+        if (cantidad >= 500) {
+            billetes500 = cantidad / 500;
+            cantidad = cantidad - (billetes500 * 500); 
+        }
+
+        
+        int billetes200 = 0;
+        if (cantidad >= 200) {
+            billetes200 = cantidad / 200;
+            cantidad = cantidad - (billetes200 * 200); 
+        }
+
+        
+        int billetes100 = 0;
+        if (cantidad >= 100) {
+            billetes100 = cantidad / 100;
+            cantidad = cantidad - (billetes100 * 100); 
+        }
+
+       
+        int billetes50 = 0;
+        if (cantidad >= 50) {
+            billetes50 = cantidad / 50;
+            cantidad = cantidad - (billetes50 * 50); 
+        }
+
+        
+        int billetes20 = 0;
+        if (cantidad >= 20) {
+            billetes20 = cantidad / 20;
+            cantidad = cantidad - (billetes20 * 20); 
+        }
+
+       
+        int billetes10 = 0;
+        if (cantidad >= 10) {
+            billetes10 = cantidad / 10;
+            cantidad = cantidad - (billetes10 * 10); 
+        }
+
+        
+        int billetes5 = 0;
+        if (cantidad >= 5) {
+            billetes5 = cantidad / 5;
+            cantidad = cantidad - (billetes5 * 5); 
+        }
+
+        
+        System.out.println("Billetes necesarios:");
+        if (billetes500 > 0) {
+            System.out.println(billetes500 + " billete(s) de 500 €");
+        }
+        if (billetes200 > 0) {
+            System.out.println(billetes200 + " billete(s) de 200 €");
+        }
+        if (billetes100 > 0) {
+            System.out.println(billetes100 + " billete(s) de 100 €");
+        }
+        if (billetes50 > 0) {
+            System.out.println(billetes50 + " billete(s) de 50 €");
+        }
+        if (billetes20 > 0) {
+            System.out.println(billetes20 + " billete(s) de 20 €");
+        }
+        if (billetes10 > 0) {
+            System.out.println(billetes10 + " billete(s) de 10 €");
+        }
+        if (billetes5 > 0) {
+            System.out.println(billetes5 + " billete(s) de 5 €");
+        }
+
+        
+        scanner.close();
+    }
+}	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 
 
