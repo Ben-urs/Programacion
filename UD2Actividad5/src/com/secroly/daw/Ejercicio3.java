14dbfb9550a6d5d1a511e000ef8d8b297156301f
COMPU-S029\daw1 <beniaminpatras@gmail.com>
2024-10-23 09:52:17 +0200

    23/10

--- UD2Actividad5/src/com/secroly/daw/Ejercicio3.java
+++ UD2Actividad5/src/com/secroly/daw/Ejercicio3.java
@@ -4,26 +4,26 @@ import java.util.Scanner;
 
 public class Ejercicio3 {
 	
+    //3-Programa que pide por teclado tu nombre y tus 2 apellidos y crea un codigo de usuario conbinando las tres primeras letras de cada una.
     public static void main(String[] args) {
     	
         Scanner scanner = new Scanner(System.in);
         
-        System.out.println("Introduce el nombre:");
-        
+        System.out.println("Introduce tu nombre:");      
         String nombre = scanner.nextLine();
         
-        System.out.println("Introduce el primer apellido:");
-        
+        System.out.println("Introduce tu primer apellido:");      
         String apellido1 = scanner.nextLine();
         
-        System.out.println("Introduce el segundo apellido:");
-        
+        System.out.println("Introduce tu segundo apellido:");       
         String apellido2 = scanner.nextLine();
         
         String codigoUsuario = nombre.substring(0, 3).toUpperCase() +
                                apellido1.substring(0, 3).toUpperCase() +
                                apellido2.substring(0, 3).toUpperCase();
         
-        System.out.println("Código de usuario: " + codigoUsuario);
+        System.out.println("El codigo del usuario es: " + codigoUsuario);
+
+        scanner.close();
     }
 }
