4582e86e328251231daf2a966fbe44b7b141890a
COMPU-S029\daw1 <beniaminpatras@gmail.com>
2024-11-25 09:56:27 +0100

    Actualizado

--- DawBank/DawBank/src/DawBank.java
+++ DawBank/DawBank/src/DawBank.java
@@ -4,27 +4,35 @@ public class DawBank {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Introduce el IBAN:");
+        System.out.println("Bienvenido a DawBank");
+        System.out.println();
+        System.out.println("Recuerda que debe comenzar por 2 letra mayusculas y 22 numeros");
+        System.out.println("Ejemplo: EL9999999999999999999999");
+        System.out.println();
+        System.out.println("Introduce tu IBAN:");
+
+        final String patronIBAN = "[A-Z]{2}[0-9]{22}";
 
         String iban = scanner.nextLine();
 
-        System.out.println("Introduce el titular:");
+        while (!iban.matches(patronIBAN)) {
+            System.out.println("IBAN invalido. Debe comenzar por 2 letras mayusculas seguidas de 22 numeros");
+            System.out.println();
+            System.out.println("Introduce un IBAN valido:");
+
+            iban = scanner.nextLine();
+        }
+
+        System.out.println("Introduce el nombre del titular de la cuenta:");
 
         String titular = scanner.nextLine();
 
-        App cuenta;
-        try {
-            cuenta = new App(iban, titular);
-
-        } catch (IllegalArgumentException e) {
-            System.out.println("Error: " + e.getMessage());
-            return;
-        }
-
+        CuentaBancaria cuenta = new CuentaBancaria(iban, titular);
+        
         int opcion;
 
         do {
-            System.out.println("Menú:");
+            System.out.println("Menu:");
             System.out.println("1. Datos de la cuenta");
             System.out.println("2. IBAN");
             System.out.println("3. Titular");
@@ -33,7 +41,7 @@ public class DawBank {
             System.out.println("6. Retirada");
             System.out.println("7. Movimientos");
             System.out.println("8. Salir");
-            System.out.print("Elige una opción: ");
+            System.out.print("Seleccione una opcion: ");
 
             opcion = scanner.nextInt();
 
@@ -44,49 +52,59 @@ public class DawBank {
                     System.out.println("Saldo: " + cuenta.getSaldo() + "€");
 
                     break;
+
                 case 2:
                     System.out.println("IBAN: " + cuenta.getIban());
 
                     break;
+
                 case 3:
                     System.out.println("Titular: " + cuenta.getTitular());
 
                     break;
+
                 case 4:
                     System.out.println("Saldo: " + cuenta.getSaldo() + "€");
 
                     break;
+
                 case 5:
                     System.out.print("Cantidad a ingresar: ");
-
+                    
                     double ingreso = scanner.nextDouble();
+
                     cuenta.ingresar(ingreso);
 
                     break;
+
                 case 6:
                     System.out.print("Cantidad a retirar: ");
 
                     double retirada = scanner.nextDouble();
+
                     cuenta.retirar(retirada);
 
                     break;
+
                 case 7:
                     System.out.println("Movimientos:");
 
-                    for (Movimiento m : cuenta.getMovimientos()) {
-                        m.mostrarInfoMovimiento();
+                    cuenta.mostrarMovimientos();
 
-                    }
                     break;
+
                 case 8:
-                    System.out.println("Saliendo del programa.");
+                    System.out.println("Saliendo del programa");
+                    System.out.println("Hasta otra");
 
                     break;
-                default:
-                    System.out.println("Opción no válida.");
-            }
 
-        } while (opcion != 8);
+                default:
+                    System.out.println("Opcion no valida, Introduzca una de las opcion mostradas en pantalla");
+            }
+        } 
+        
+        while (opcion != 8);
 
         scanner.close();
     }
