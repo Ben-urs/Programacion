8d3fbf360b092357b3d1d3b3e4c07bdf49d7456f
Ben-urs <beniaminpatras@gmail.com>
2025-01-20 20:02:43 +0100

    Update ProgramaAgenda.java

--- UD3_Actividad_04_herencias_02/src/ProgramaAgenda.java
+++ UD3_Actividad_04_herencias_02/src/ProgramaAgenda.java
@@ -2,79 +2,109 @@ import java.util.Scanner;
 
 public class ProgramaAgenda {
     public static void main(String[] args) {
-        Agenda agenda = new Agenda();
         Scanner scanner = new Scanner(System.in);
-        int opcion;
+        Agenda agenda = new Agenda(); 
 
-        do {
-            System.out.println("\n--- Agenda ---");
-            System.out.println("\n1. Añadir contacto");
-            System.out.println("2. Eliminar contacto");
-            System.out.println("3. Listar contactos");
-            System.out.println("4. Buscar contacto");
-            System.out.println("5. Salir");
-            System.out.print("\nSeleccione una opcion: ");
-            opcion = scanner.nextInt();
+        System.out.println("\nAgenda");
+
+        boolean continuar = true;
+
+        while (continuar) {
+            mostrarMenu();
+            int opcion = scanner.nextInt();
 
             scanner.nextLine(); 
 
             switch (opcion) {
                 case 1:
-                    if (agenda.añadirContacto()) {
-                        System.out.println("\nContacto añadido.");
-                    } 
-                    
-                    else {
-                        System.out.println("\nEl contacto ya existe.");
-                    }
-                    
+                    agregarContacto(agenda, scanner);
+
                     break;
 
                 case 2:
-                    System.out.print("\nIngresa el nombre del contacto que quieres eliminar: ");
-                    String nombreEliminar = scanner.nextLine();
+                    eliminarContactoPorNombre(agenda, scanner);
 
-                    if (agenda.eliminarContacto(nombreEliminar)) {
-                        System.out.println("\nContacto eliminado");
-                    } 
-                    
-                    else {
-                        System.out.println("\nEl contacto no existe");
-                    }
-                    
                     break;
 
                 case 3:
-                    System.out.println("\nLista de contactos:");
-                    agenda.listarContactos();
-                    
+                    listarContactos(agenda);
+
                     break;
 
                 case 4:
-                    System.out.print("\nIngrese el nombre del contacto a buscar: ");
-                    String nombreBuscar = scanner.nextLine();
-                    int posicion = agenda.buscaContacto(nombreBuscar);
-                    
-                    if (posicion != -1) {
-                        System.out.println("\nContacto encontrado en la: " + posicion + "º posicion");
-                    } 
-                    
-                    else {
-                        System.out.println("\nEl contacto no existe");
-                    }
+                    buscarContactoPorNombre(agenda, scanner);
 
                     break;
 
                 case 5:
-                    System.out.println("\nSaliendo de la agenda");
+                    continuar = false;
 
                     break;
 
                 default:
-                    System.out.println("\nOpcion no valida. Intente de nuevo");
+                    System.out.println("Opcion no valida");
             }
-        } while (opcion != 5);
-
+        }
+        System.out.println("\nSaliendo de la agenda\n");
         scanner.close();
+    }
+
+    private static void mostrarMenu() {
+        System.out.println("\n1. Añadir contacto");
+        System.out.println("2. Eliminar contacto");
+        System.out.println("3. Listar contactos");
+        System.out.println("4. Buscar contacto");
+        System.out.println("5. Salir");
+        System.out.print("\nSeleccione una opcion: ");
+    }
+
+    private static void agregarContacto(Agenda agenda, Scanner scanner) {
+        System.out.print("Nombre: ");
+        String nombre = scanner.nextLine();
+
+        System.out.print("Telefono: ");
+        String telefono = scanner.nextLine();
+
+        Contacto contacto = new Contacto(nombre, telefono);
+
+        if (agenda.añadirContacto(contacto)) {
+            System.out.println("Contacto añadido");         
+        } 
+        
+        else {
+            System.out.println("No se pudo añadir el contacto");
+        }
+    }
+
+    private static void eliminarContactoPorNombre(Agenda agenda, Scanner scanner) {
+        System.out.print("Nombre del contacto para eliminarlo: ");
+        String nombreEliminar = scanner.nextLine();
+
+        if (agenda.eliminarContacto(nombreEliminar)) {
+            System.out.println("Contacto eliminado");
+        } 
+        
+        else {
+            System.out.println("No se encontro el contacto");
+        }
+    }
+
+    private static void listarContactos(Agenda agenda) {
+        System.out.println("\nLista de contactos:");
+        agenda.listarContactos();
+    }
+
+    private static void buscarContactoPorNombre(Agenda agenda, Scanner scanner) {
+        System.out.print("\nNombre del contacto: ");
+        String nombreBuscar = scanner.nextLine();
+        
+        int posicion = agenda.buscaContacto(nombreBuscar);
+        if (posicion != -1) {
+            System.out.println("\nContacto encontrado en la: " + posicion + "º posicion");
+        } 
+        
+        else {
+            System.out.println("\nContacto no encontrado");
+        }
     }
 }
